9b14cdb4a24301ebfdf7c973425e40fc1d8965cf
GuilhermeShun <akashi.guilherme@aluno.ifsp.edu.br>
2024-09-04 10:10:06 -0300

    Teste ifds

---
+++ script.js
@@ -0,0 +1,7 @@
+//criando uma variável (meu pau no seu burrote)
+let mensagem1 = "Bom dia, estamos aprendendo";
+let mensagem2 = "JavaScript";
+
+//exibindo o resultado
+console.log(mensagem1);
+console.log(mensagem2);
